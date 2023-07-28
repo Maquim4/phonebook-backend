767ff3a9eba4eb228e9ca41efc1b413edf844560
Andrey <konohov.andreyka@gmail.com>
2023-07-28 12:32:49 +0300

    fix: post route

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
